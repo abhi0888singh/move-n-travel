29c68f6aa838029e52f51c8747bc3174e9746c67
abhi0888singh <abhijeet23009@gmail.com>
2026-01-30 22:29:54 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,11 +1,17 @@
 import { auth } from "./firebase.js";
-import { onAuthStateChanged, signOut } from
-  "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
+import {
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+const emailText = document.getElementById("userEmail");
 const logoutBtn = document.getElementById("logoutBtn");
 
 onAuthStateChanged(auth, (user) => {
-  if (!user) {
+  if (user) {
+    emailText.innerHTML = "Welcome: " + user.email;
+  } else {
     window.location.href = "login.html";
   }
 });
