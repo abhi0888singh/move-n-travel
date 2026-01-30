d1a4d999a5a88f47f039caf4eaea5d78dc1f6f68
abhi0888singh <abhijeet23009@gmail.com>
2026-01-30 22:22:51 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,28 +1,20 @@
 // js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
-import {
-  getAuth
-} from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
 
-import {
-  getFirestore
-} from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
-
-// Your Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSyCGsWteoWmrvy2tvJ-s7LEwrrzzddvobcJY",
-  authDomain: "move-n-travel.firebaseapp.com",
-  projectId: "move-n-travel",
-  storageBucket: "move-n-travel.appspot.com",
-  messagingSenderId: "558385612046",
-  appId: "1:558385612046:web:1e54375d890b208fdec35f"
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export Auth + Firestore
 export const auth = getAuth(app);
 export const db = getFirestore(app);
