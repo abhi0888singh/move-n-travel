e77ed437bca1229ad29676d97b99e8442fd4dc1a
abhi0888singh <abhijeet23009@gmail.com>
2026-01-30 22:29:32 +0530

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -1,6 +1,6 @@
 import { auth } from "./firebase.js";
-import { createUserWithEmailAndPassword } from
-  "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const form = document.getElementById("signupForm");
 const msg = document.getElementById("signupMsg");
@@ -13,15 +13,15 @@ form.addEventListener("submit", (e) => {
 
   createUserWithEmailAndPassword(auth, email, password)
     .then(() => {
+      msg.innerHTML = "✅ Signup Successful!";
       msg.style.color = "lime";
-      msg.innerText = "Account Created ✅ Redirecting...";
 
       setTimeout(() => {
         window.location.href = "login.html";
       }, 1500);
     })
-    .catch((error) => {
+    .catch((err) => {
+      msg.innerHTML = "❌ " + err.message;
       msg.style.color = "red";
-      msg.innerText = error.message;
     });
 });
