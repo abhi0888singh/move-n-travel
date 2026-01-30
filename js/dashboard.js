00dfd574377b625cc650d6cb7515248f75314861
abhi0888singh <abhijeet23009@gmail.com>
2026-01-30 22:10:19 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,29 +3,21 @@ import { auth } from "./firebase.js";
 import {
   onAuthStateChanged,
   signOut
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
 
-const userEmail = document.getElementById("userEmail");
+const userText = document.getElementById("userEmail");
 const logoutBtn = document.getElementById("logoutBtn");
 
-// ✅ Protect Dashboard
 onAuthStateChanged(auth, (user) => {
   if (user) {
-    userEmail.innerText = user.email;
+    userText.innerHTML = "Welcome: " + user.email;
   } else {
-    alert("❌ Please Login First!");
     window.location.href = "login.html";
   }
 });
 
-// ✅ Logout
 logoutBtn.addEventListener("click", () => {
-  signOut(auth)
-    .then(() => {
-      alert("✅ Logged Out Successfully!");
-      window.location.href = "login.html";
-    })
-    .catch((error) => {
-      alert("Error: " + error.message);
-    });
+  signOut(auth).then(() => {
+    window.location.href = "login.html";
+  });
 });
