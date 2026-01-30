e98a724596d3e1d79b2b900aee2f193bbf8a0d94
abhi0888singh <abhijeet23009@gmail.com>
2026-01-30 22:08:34 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,5 @@
+// js/firebase.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
 
 import {
@@ -8,16 +10,19 @@ import {
   getFirestore
 } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
 
+// Your Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSyCGsWteoWmrvy2tvJ-s7LEwrrzddvobcJY",
+  apiKey: "AIzaSyCGsWteoWmrvy2tvJ-s7LEwrrzzddvobcJY",
   authDomain: "move-n-travel.firebaseapp.com",
   projectId: "move-n-travel",
-  storageBucket: "move-n-travel.firebasestorage.app",
+  storageBucket: "move-n-travel.appspot.com",
   messagingSenderId: "558385612046",
   appId: "1:558385612046:web:1e54375d890b208fdec35f"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Export Auth + Firestore
 export const auth = getAuth(app);
 export const db = getFirestore(app);
