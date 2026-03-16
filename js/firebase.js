ef55abae710e0770b9d5ea10fe4661df1bba50b4
abhi0888singh <abhijeet23009@gmail.com>
2026-03-16 17:16:05 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,16 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGsWteoWmrvy2tvJ-s7LEwrrzddvobcJY",
-  authDomain: "move-n-travel.firebaseapp.com",
-  projectId: "move-n-travel",
-  storageBucket: "move-n-travel.firebasestorage.app",
-  messagingSenderId: "558385612046",
-  appId: "1:558385612046:web:1e54375d890b208fdec35f",
-  measurementId: "G-CZFGVW2FTL"
+
+apiKey:"YOUR_KEY",
+authDomain:"YOUR_PROJECT.firebaseapp.com",
+projectId:"YOUR_PROJECT_ID",
+appId:"APP_ID"
+
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
